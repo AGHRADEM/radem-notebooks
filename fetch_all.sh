702e9739fdd2171c4f394dc142d517ca8f0a217a
Szymon Bednorz <dsonyyy@gmail.com>
2024-08-16 03:40:03 +0200

    added preliminary notebook to compute radem trajectory

---
+++ fetch_all.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+echo "============= FETCHING RADEM =============" 
+cd processing
+./0_fetch_data.sh
+cd ..
+
+echo "============= FETCHING IREM =============" 
+cd irem
+./0_fetch_data.sh
+cd ..
+
+echo "============= FETCHING KERNELS =============" 
+cd kernel
+./0_fetch_data.sh
+cd ..
+
+echo ok
--- kernel/0_fetch_data.sh
+++ kernel/0_fetch_data.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-URL=https://spiftp.esac.esa.int/data/SPICE/JUICE/kernels/
+URN=spiftp.esac.esa.int/data/SPICE/JUICE/kernels/
+URI=https://${URN}
 DATA_DIR="../data/kernels"
+META_KERNEL_NAME="juice_crema_5_1_150lb_23_1_a3.tm"
 
 mkdir -p ${DATA_DIR}
 
@@ -11,5 +13,23 @@ wget \
     --no-parent \
     --continue \
     --no-clobber \
-    ${URL} \
+    ${URI} \
     -P ${DATA_DIR}
+
+
+# WORKAROUND: 
+# creating a symlink to shorten the absolute path because 
+# spiceypy (or underlying SPICE utils) cuts off the path 
+# at >~100 characters.
+KERNELS_PATH=$(pwd)/${DATA_DIR}/${URN}
+KERNELS_SYM_PATH=$(pwd)/${DATA_DIR}/kernels
+if [ ! -L ${KERNELS_SYM_PATH} ]; then
+    ln -s ${KERNELS_PATH} ${KERNELS_SYM_PATH}
+fi
+
+# as recommended by SPICE:
+# set meta kernel's PATH_VALUES and create its backup
+META_KERNEL_PATH=${KERNELS_SYM_PATH}/mk/${META_KERNEL_NAME}
+PATH_VALUES_OLD="PATH_VALUES       = ( '..' )"
+PATH_VALUES="PATH_VALUES       = ( '${KERNELS_SYM_PATH}' )"
+sed -i.bak "s|${PATH_VALUES_OLD}|${PATH_VALUES}|" ${META_KERNEL_PATH}
