a746e0dbaeff055253c4830064e52d9613c19244
Szymon Bednorz <dsonyyy@gmail.com>
2024-09-23 06:04:57 +0200

    fixes for installation pipeline

--- fetch_all.sh
+++ fetch_all.sh
@@ -5,14 +5,14 @@ cd processing
 ./0_fetching_ftp.sh
 cd ..
 
-echo "============= FETCHING IREM =============" 
-cd irem
-./0_fetch_data.sh
-cd ..
+# echo "============= FETCHING IREM =============" 
+# cd irem
+# ./0_fetch_data.sh
+# cd ..
 
-echo "============= FETCHING KERNELS =============" 
-cd kernel
-./0_fetch_data.sh
-cd ..
+# echo "============= FETCHING KERNELS =============" 
+# cd kernel
+# ./0_fetch_data.sh
+# cd ..
 
 echo ok
